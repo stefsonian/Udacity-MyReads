a8a11a2a2e9de85f974626408be322c32c810baf
Stefan Stefansen <mail@stefanstefansen.com>
2017-08-07 15:59:46 +0200

    Move nav to React Router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
-import React from 'react'
-import * as BooksAPI from './BooksAPI'
-import './App.css'
-import Books from './components/Books'
-import Search from './components/Search'
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
+import "./App.css";
+import Books from "./components/Books";
+import Search from "./components/Search";
 
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+class BooksApp extends Component {
+  state = {};
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <Search />
-        ) : (
-          <Books />
-        )}
+        <Route path="/" exact={true} component={Books} />
+        <Route path="/search" component={Search} />
       </div>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom'
 
 class Search extends Component {
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a
+          <Link
+            to="/"
             className="close-search"
-            onClick={() => this.setState({ showSearchPage: false })}
           >
             Close
-          </a>
+          </Link>
           <div className="search-books-input-wrapper">
             {/*
               NOTES: The search from BooksAPI is limited to a particular set of search terms.
